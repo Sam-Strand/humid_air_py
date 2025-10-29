d4b1d3a2405fdf1cab134be15aaabb404cf09df6
Sam-Strand <i@maxim-sadovskiy.ru>
2025-10-29 21:36:11 +0300

    Подготовка к релизу

--- humid_air_py/__init__.py
+++ humid_air_py/__init__.py
@@ -4,6 +4,6 @@
 
 from .core import Humid_air
 
-__version__ = "0.1.0"
+__version__ = "1.0.2"
 __author__ = "Садовский М.К."
 __all__ = ["Humid_air"]
--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="humid_air_py",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),
+    install_requires=[
+        "numpy>=1.20.0",
+        "numba>=0.55.0"
+    ]
 )
