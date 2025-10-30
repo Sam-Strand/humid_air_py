598b76073f25051a4374385fe3a9ee8d02112f5f
Maxim <i@maxim-sadovskiy.ru>
2025-10-30 14:56:37 +0300

    Лицензия

--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@ from setuptools import setup, find_packages
 setup(
     name="humid_air_py",
     version="1.0.2",
+    url="https://github.com/Sam-Strand/humid_air_py",
+    author="Садовский М.К.",
+    author_email="i@maxim-sadovskiy.ru",
     packages=find_packages(),
     install_requires=[
         "numpy>=1.20.0",
         "numba>=0.55.0"
-    ]
+    ],
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+    ],
 )
